50df38dda90358987574526f0b296aa10057c747
Robert Ludwick <rludwick@drew.edu>
2019-03-18 14:12:56 -0400

    added actual table and hard coded some data into it

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,28 +1,44 @@
 import React, { Component } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 
 class Table extends Component {
-    state = { 
-        count : 0
-     };
+    constructor(props) {
+      super(props);
+      this.state = {
+        columnDefs: [{
+          headerName: "Time", field: "time"
+        }, {
+          headerName: "Monday", field: "monday"
+        }, {
+          headerName: "Tuesday", field: "tuesday"
+        }],
+        rowData: [{
+          time: "8:45", monday: "Vaughn", tuesday: "Ida"
+        }, {
+          time: "9:00", monday: "Vaughn", tuesday: "Ida"
+        }, {
+          time: "9:15", monday: "Vaughn", tuesday: "Ian"
+        }]
+      }
+    }
 
-    render() { 
-        
-       
-        
-        return (  
-            <h1>
-                 {this.tableCount()}
-            </h1>
+    render() {
+        return (
+          <div 
+            className="ag-theme-balham"
+            style={{ 
+            height: '500px', 
+            width: '600px' }} 
+          >
+            <AgGridReact
+              columnDefs={this.state.columnDefs}
+              rowData={this.state.rowData}>
+            </AgGridReact>
+          </div>
         );
-
-        
+      }
     }
-
-    tableCount() {
-        var { count } = this.state;
-        count = count +1;
-        return count;
-    }
-}
- 
-export default Table;
+    
+    export default Table;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import TableExampleSimple from './components/Table'; 
+import Table from './components/Table';
 
-ReactDOM.render(<TableExampleSimple />, document.getElementById('root'));
+ReactDOM.render(<Table />, document.getElementById('root'));
 serviceWorker.unregister();
