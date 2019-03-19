930b9d470bf9df8b9959e0cce0d897a2a3ab5865
Robert Ludwick <rludwick@drew.edu>
2019-03-18 23:43:05 -0400

    resolve merge conflicts

--- src/index.js
+++ src/index.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-//import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import "bootstrap/dist/css/bootstrap.css";
-import Tester from "./compTester"
-
-
-ReactDOM.render(<Tester />, document.getElementById("root"));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-=======
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -22,5 +6,4 @@ import * as serviceWorker from './serviceWorker';
 import Table from './components/Table';
 
 ReactDOM.render(<Table />, document.getElementById('root'));
->>>>>>> schedule-display
 serviceWorker.unregister();
