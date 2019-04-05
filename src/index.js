29123c36fb412876c4a6d19b4d34c3b7631d9d7d
Robert Ludwick <rludwick@drew.edu>
2019-04-04 21:51:45 -0400

    Heroku support

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Table from './components/Table';
-import ControlPanel from './components/ControlPanel';
 
 ReactDOM.render(<Table />, document.getElementById('table'));
-ReactDOM.render(<ControlPanel />,document.getElementById('root'))
 serviceWorker.unregister();
