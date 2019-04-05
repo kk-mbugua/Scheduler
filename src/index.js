0531ef0025066723443af77981fcd5117328d760
Robert Ludwick <rludwick@drew.edu>
2019-04-05 15:35:36 -0400

    merge corrections

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
 import './frontend/page/index.css';
 //import App from './frontend/App';
 import * as serviceWorker from './frontend/page/serviceWorker';
@@ -9,12 +8,3 @@ import Tester from "./frontend/page/compTester"
 
 ReactDOM.render(<Tester />, document.getElementById('table'));
 serviceWorker.unregister();
-=======
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import Table from './components/Table';
-
-ReactDOM.render(<Table />, document.getElementById('table'));
-serviceWorker.unregister();
->>>>>>> backend
