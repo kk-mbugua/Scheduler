e1ff8a6f17b23758eb3912eb996814b33d259880
Robert Ludwick <rludwick@drew.edu>
2019-04-18 14:54:10 -0400

    merge conflicts

--- src/database.js
+++ src/database.js
@@ -68,6 +68,7 @@ const deleteUser = (request, response) => {
   })
 }
 
+
 module.exports = {
   getUsers,
   getUserById,
--- src/index.js
+++ src/index.js
@@ -1,42 +1,10 @@
-<<<<<<< HEAD
-const express = require('express')
-const bodyParser = require('body-parser')
-const db = require('./database')
-const app = express()
-const port = 3000
-=======
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './frontend/page/index.css';
-import App from './frontend/page/App';
+//import App from './frontend/App';
 import * as serviceWorker from './frontend/page/serviceWorker';
-//import Tester from "./frontend/page/compTester"
->>>>>>> 701f4c800f48793ce158a5630c63468a20dc69d5
-
-app.use(bodyParser.json())
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-)
-
-<<<<<<< HEAD
+import Tester from "./frontend/page/compTester"
 
 
-app.get('/users', db.getUsers)
-app.get('/users/:id', db.getUserById)
-app.post('/users', db.createUser)
-app.put('/users/:id', db.updateUser)
-app.delete('/users/:id', db.deleteUser)
-
-app.get('/', (request, response) => {
-    response.json({ info: 'Node.js, Express, and Postgres API' })
-  })
-
-app.listen(port, () => {
-    console.log(`App running on port ${port}.`)
-})
-=======
-ReactDOM.render(<App />, document.getElementById('table'));
+ReactDOM.render(<Tester />, document.getElementById('table'));
 serviceWorker.unregister();
->>>>>>> 701f4c800f48793ce158a5630c63468a20dc69d5
