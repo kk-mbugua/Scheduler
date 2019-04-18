7cb857822404e3b1eb90b92ea28be122169c6b81
Robert Ludwick <rludwick@drew.edu>
2019-04-18 03:57:19 -0400

    Database Api

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './frontend/page/index.css';
-//import App from './frontend/App';
-import * as serviceWorker from './frontend/page/serviceWorker';
-import Tester from "./frontend/page/compTester"
+const express = require('express')
+const bodyParser = require('body-parser')
+const db = require('./database')
+const app = express()
+const port = 3000
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
 
 
-ReactDOM.render(<Tester />, document.getElementById('table'));
-serviceWorker.unregister();
+
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
+
+app.get('/', (request, response) => {
+    response.json({ info: 'Node.js, Express, and Postgres API' })
+  })
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+})
