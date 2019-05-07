6cdf00b5c13723036e2686abed8cac820a84d98c
Kibugi Kamau Mbugua <mbg.kamau@gmail.com>
2019-05-07 12:02:39 -0400

    fixed package.json

--- src/frontend/page/App.js
+++ src/frontend/page/App.js
@@ -4,24 +4,19 @@ import NavBar from "../components/navbar/NavBar";
 import LoginScene from "../scenes/loginScene";
 import TimeInputScene from "../scenes/timeInputScene";
 import PageNotFoundScene from "../scenes/pageNotFoundScene";
-import Table from "../components/Table";
-import Test from "../components/tableTest";
-import Schedule from "../components/schedule/schedule";
 import AdminHomeScene from "../scenes/adminHomeScene";
 import EmployeesScene from "../scenes/employeesScene";
 import AdminScheduleScene from "../scenes/adminScheduleScene";
 import PositionsScene from "../scenes/positionsScenes";
-//const history = createBrowserHistory()
+
 
 class App extends Component {
   state = {
     isLoggedIn: false
   };
 
-
   setLogin = profile => {
-    // localStorage.setItem("role", profile.role);
-    localStorage.setItem("role", "employee");
+    localStorage.setItem("role", profile.role);
     localStorage.setItem("email", profile.email);
     localStorage.setItem("isLoggedIn", profile.isLoggedIn);
     this.setState({ isLoggedIn: profile.isLoggedIn });
@@ -66,9 +61,7 @@ class App extends Component {
       <BrowserRouter>
         {this.rednerNavBar()}
         <Switch>
-          <Route path="/" component={Table} exact />
-          <Route path="/test" component={Test} exact />
-          <Route path="/display" component={Schedule} exact />
+          <Route path="/" component={TimeInputScene} exact />
           <Route path="input" component={TimeInputScene} />
           <Route component={PageNotFoundScene} />
         </Switch>
@@ -77,7 +70,7 @@ class App extends Component {
     return comp;
   };
   render() {
-    console.log("Are you logged in? ", localStorage.getItem("isLoggedIn"))
+    console.log("Are you logged in? ", localStorage.getItem("isLoggedIn"));
     if (localStorage.getItem("isLoggedIn") === "false") {
       return <LoginScene setLogin={this.setLogin} />;
     }
